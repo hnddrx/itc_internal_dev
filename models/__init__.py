ffdaf11a62f70f9e429174e3fc9df9b201a1f5a9
hnddrx <macayanwren@gmail.com>
2026-03-13 10:24:37 +0800

    feat: added disbursement voucher module

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from . import salary_advance
-from . import cash_advance
+from . import cash_advance, disbursement_voucher
 from . import hr_expense
 from . import purchase_order
 from . import account_account
